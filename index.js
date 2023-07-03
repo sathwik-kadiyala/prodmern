761890c76f80c5752b15bbc5fea558460d05029d
sathwik-kadiyala <sathwikkadiyala@gmail.com>
2023-07-03 18:42:38 +0530

    second commit

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use( express.static(path.join(__dirname, './client/build')));
+app.use( express.static(path.join(__dirname, "./client/build")));
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 
 app.get("*", function(req,res){
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
 /* FILE STORAGE */
